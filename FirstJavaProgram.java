7b485bc719ab776cd07fcb3252ad911b24aa34bd
caden326 <84342510+caden326@users.noreply.github.com>
2021-08-02 14:26:13 -0500

    Add files via upload

---
+++ FirstJavaProgram.java
@@ -0,0 +1,8 @@
+public class FirstJavaProgram{
+    public static void main(String[] args) {
+        String name = "Caden";
+        String home = "New Orleans, LA";
+        int age = 20;
+        System.out.println("My name is "+ name + " I am " + age + " My Hometown is " + home);
+    }
+}
